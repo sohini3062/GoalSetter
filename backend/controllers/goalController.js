428b7b7dd51c16e89dd674ad7266344584ca67e1
Sohini Bhattacharya <bhattacharyasohini48@gmail.com>
2023-01-25 00:02:05 +0530

    Required changes

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,46 @@
+// @desc Get goals 
+// @route GET/api/goals
+// @access Private 
+
+const getGoals=(req,res)=>{
+   res.status(200).json({message:"Get goals"})
+}
+
+// @desc Set goals 
+// @route POST/api/goals
+// @access Private 
+
+const setGoal=(req,res)=>{
+  if(!req.body.text){
+      res.status(400).json({message: "add a text field"})
+      throw new Error('Please add a text')
+  }
+ 
+   res.status(200).json({message:"Set goals"})
+}
+
+
+// @desc Update goal
+// @route PUT/api/goals/id
+// @access Private 
+const updateGoal = (req, res) => {
+
+  res.status(200).json({message:`Update goal ${req.params.id}`})
+}
+
+// @desc Delete goal
+// @route PUT/api/goals/id
+// @access Private 
+const deleteGoal = (req, res) => {
+
+  res.status(200).json({message:`Delete goal ${req.params.id}`})
+}
+
+// @desc Get goals 
+// @route GET/api/goals
+// @access Private 
+
+
+module.exports = {
+  getGoals,setGoal,updateGoal,deleteGoal
+}
